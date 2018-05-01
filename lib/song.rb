5fdbe0ebf9093b07f8b086556dc91699dec025a3
Megan Collier <meganraec@live.com>
2018-05-01 04:44:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   def self.create_from_filename(file_name)
     song = self.create
     song.name = file_name.split(/ - |.mp3/)[1]
-    artist_name = file_name.split(/ - |.mp3/)[0]
+    song.artist_name = file_name.split(/ - |.mp3/)[0]
   end
 
   def self.destroy_all
